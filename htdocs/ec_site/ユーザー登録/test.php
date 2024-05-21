9d68b0cc085d476055700ab9a0df1abb49f9b43f
nakataitex <nakatahirokiitex@gmail.com>
2024-05-21 13:48:16 +0900

    フォルダの整理行う前

--- htdocs/ec_site/ユーザー登録/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-function form_check($data)
-{
-    $match_value = '/^[A-Za-z0-9]+$/';
-    foreach ($data as $key => $value) {
-        echo "キー: " . $key . ", 値: " . $value . "\n";
-        echo "$data";
-    }
-}
