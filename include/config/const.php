d522b8834456a9e317672b6c089181be51e05d66
nakataitex <nakatahirokiitex@gmail.com>
2024-05-28 10:36:46 +0900

    Update const.php

--- include/config/const.php
+++ include/config/const.php
@@ -1,8 +1,8 @@
 <?
 //定数を記述
-define('DSN', 'mysql:host=localhost;port=8889;dbname=ec_site');
-define('LOGIN_USER', 'root');
-define('LOGIN_PASSWORD', 'root');
+define('DSN', 'mysql:host=localhost;dbname=xb513874_u338x');
+define('LOGIN_USER', 'xb513874_fpu2g');
+define('LOGIN_PASSWORD', 'mj3mt8vtwv');
 
 //商品画像の保存先
 define('IMG_DIR', './assets/img/');
