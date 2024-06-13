d9b70a8e9569a929bec3b9f1181406e5f48e3722
nakataitex <nakatahirokiitex@gmail.com>
2024-06-13 14:16:00 +0900

    ログイン、ユーザー登録時のメッセージ変更
    半角英数で8文字以上→半角英数またはアンダースコア8文字以上

--- include/view/register_view.php
+++ include/view/register_view.php
@@ -3,13 +3,13 @@
     <form action="" method="POST">
         <dl>
             <div class="form-group">
-                <dt>ユーザー名：<span class="required">必須</span>(半角英数で5文字以上)</dt>
+                <dt>ユーザー名：<span class="required">必須</span>(半角英数またはアンダースコア5文字以上)</dt>
                 <dd>
                     <input type="text" name="user_name" pattern="[A-Za-z0-9_]+" minlength="5" required />
                 </dd>
             </div>
             <div class="form-group">
-                <dt>パスワード：<span class="required">必須</span>(半角英数で8文字以上)</dt>
+                <dt>パスワード：<span class="required">必須</span>(半角英数またはアンダースコア8文字以上)</dt>
                 <dd>
                     <input type="password" name="password" pattern="[A-Za-z0-9_]+" minlength="8" required />
                 </dd>
